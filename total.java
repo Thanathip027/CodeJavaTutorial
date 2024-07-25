b5fa35ff1c2b38bbd1f53d2273da66473045c6af
Thanathip027 <s66309010027@kktech.ac.th>
2024-07-25 14:30:32 +0700

    Create total.java

---
+++ total.java
@@ -0,0 +1,9 @@
+public class total {
+    public static void main(String args[]){
+        int a,b;
+        a = 62;
+        b = 99;
+        int total = a+b;
+        System.out.println("62+99 = " + total);
+    }
+}
