cdadfee1f5559b47ab067526cdc4378e64dddd5c
Thanathip027 <s66309010027@kktech.ac.th>
2024-08-01 14:19:57 +0700

    Create ExNo3.java

---
+++ ExNo3.java
@@ -0,0 +1,8 @@
+public class ExNo3 {
+    public static void main(String[] args) {
+        int multiplier = 8;
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(multiplier + " x " + i + " = " + (multiplier * i));
+        }
+    }
+}
