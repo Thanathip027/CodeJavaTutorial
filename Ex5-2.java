74a98b84653db2555386f2153c57eb3b2979c744
Thanathip027 <s66309010027@kktech.ac.th>
2024-08-05 10:21:14 +0700

    Update Ex5-2.java

--- Ex5-2.java
+++ Ex5-2.java
@@ -1,10 +1,7 @@
 public class Ex5-2 {
     public static void main(String[] args) {
-       
-        for (int x = 1; x <= 10; x++) {
-           
-            int cube = x * x * x;
-             
+        for (int x = 1; x <= 10; x++) { 
+            int cube = x * x * x;        
             System.out.println("The cube of " + x + " is: " + cube);
         }
     }
