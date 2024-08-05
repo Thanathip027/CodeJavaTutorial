7c84b4a8643839b76f7a5ad1e2117e8da0a3afa1
Thanathip027 <s66309010027@kktech.ac.th>
2024-08-05 11:29:51 +0700

    Update Arraylntita.java

--- Arraylntita.java
+++ Arraylntita.java
@@ -1,3 +1,8 @@
 public class Arraylntita {
-    
+    public static void main(String[] args) {
+        int[] days = {31,28,31,30,31,30,31,31,30,31,30,31};
+        for(int index = 0; index < days.length; index++){
+            System.out.println("mounth "+ (index + 1) + " has " + days[index] + " days");
+        }
+    }
 }
