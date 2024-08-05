1312a7104ba0bac936e0524318c053b08257cb34
Thanathip027 <s66309010027@kktech.ac.th>
2024-08-05 10:19:30 +0700

    Update Ex5-1.java

--- Ex5-1.java
+++ Ex5-1.java
@@ -7,7 +7,6 @@ public class Ex5-1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-       
         System.out.print("Enter the base of the triangle: ");
         double base = scanner.nextDouble();
 
