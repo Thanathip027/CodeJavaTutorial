1a3beba339bf3f15820c6bd14dba94c8d18117b3
Thanathip027 <s66309010027@kktech.ac.th>
2024-08-09 12:18:56 +0700

    Create Student1.java

---
+++ Student1.java
@@ -0,0 +1,20 @@
+public class Student1 {
+    private String name;
+    private String email;
+    public Student1(){
+        name = "Unassigned";
+        email = "Unassigned";
+    }
+    public void setName(String Studen_1Name){
+        name = Studen_1Name;
+    }
+    public void setEmail(String address){
+        email = address;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+}
